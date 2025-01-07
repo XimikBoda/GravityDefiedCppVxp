7eaf80b9ee2ec9af73a6e13492c24e274e55c072
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-01-07 22:07:00 +0200

    Added png decoder

--- src/lcdui/Image.cpp
+++ src/lcdui/Image.cpp
@@ -6,42 +6,65 @@
 #include <vmgraph.h>
 #include <vmres.h>
 
+#define STBI_ONLY_PNG
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+const VMUINT16 trans_color = VM_COLOR_888_TO_565(255, 0, 255);
+
 Image::Image(int width, int height)
 {
-    this->surface = (VMUINT8*)vm_graphic_create_canvas(width, height);
+	this->surface = (VMUINT8*)vm_graphic_create_canvas(width, height);
 
-    this->width = width;
-    this->height = height;
+	this->width = width;
+	this->height = height;
 }
 
 Image::Image(const std::string& embeddedPath)
 {
-    VMINT size = 0;
-    VMUINT8* res = vm_load_resource((char*)embeddedPath.c_str(), &size);
-    this->surface = (VMUINT8*)vm_graphic_load_image(res, size);
-    vm_free(res);
+	VMINT size = 0;
+	VMUINT8* res = vm_load_resource((char*)embeddedPath.c_str(), &size);
 
-    frame_prop *prop = vm_graphic_get_img_property((VMINT)this->surface, 1);
-    this->width = prop->width;
-    this->height = prop->height;
+	int n;
+	stbi_uc* im = stbi_load_from_memory(res, size, &width, &height, &n, 4);
+
+	this->surface = (VMUINT8*)vm_graphic_create_canvas(width, height);
+
+	VMUINT16 color;
+	for (int i = 0; i < width * height; ++i) {
+		stbi_uc* p = im + i * 4;
+		if (p[3] != 255)
+			color = trans_color;
+		else {
+			color = VM_COLOR_888_TO_565(p[0], p[1], p[2]);
+			if (color == trans_color)
+				color--;
+		}
+
+		((VMUINT16*)(surface + VM_CANVAS_DATA_OFFSET))[i] = color;
+	}
+	vm_free(res);
+	stbi_image_free(im);
+
+	vm_graphic_canvas_set_trans_color((VMINT)this->surface, trans_color);
 }
 
 Image::~Image()
 {
-    vm_graphic_release_canvas((VMINT)this->surface);
+	vm_graphic_release_canvas((VMINT)this->surface);
 }
 
 int Image::getWidth() const
 {
-    return this->width;
+	return this->width;
 }
 
 int Image::getHeight() const
 {
-    return this->height;
+	return this->height;
 }
 
 VMUINT8* Image::getSurface() const
 {
-    return this->surface;
+	return this->surface;
 }
